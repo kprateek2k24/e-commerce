4ccbdfaf0be3fb36630e89d5cfb0bfb54f278cf3
pradeepWxm <pradeep.kumar@workxmate.com>
2024-10-18 22:13:38 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Observer, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'e_commerce_v16';
+  marks:number=10;
+
+  myObservable=new Observable((observer: Observer<any>)=>{
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.complete();
+  })
+  constructor(){
+    this.myObservable.subscribe({
+      next: (value)=>{
+        console.log(value);
+      },
+      complete: ()=>{
+        console.log("Complated")
+      }
+    })
+  }
 }
+
